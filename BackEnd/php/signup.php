70f4a0137c18fc2de943c5a0363eec6049afeb32
ali-turabi <159777576+ali-turabi@users.noreply.github.com>
2024-06-10 23:10:01 +0300

    adding verification for the page signup

---
+++ BackEnd/php/signup.php
@@ -0,0 +1,15 @@
+<?php
+header("Location: ../../FrontEnd/html/index.html");
+exit; // Ensure that no other code is executed after the redirect
+//$conn = @new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+//$qry="select * from roles";
+//$res=$conn->query($qry);
+//for($i=0; $i< $res->num_rows;$i++) {
+//    $resRow = $res->fetch_object();
+//    echo $resRow->ID;
+//    echo "<br>";
+//    echo $resRow->name;
+//    echo "<br>";
+//}
+echo 'ali turabi love bmw m3';
+?>
